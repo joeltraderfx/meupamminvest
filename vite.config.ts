e61f99a8d767ddcdb7f588e50021ec4438248ab0
joeltraderfx <141392129+joeltraderfx@users.noreply.github.com>
2026-07-15 05:29:55 -0300

    Adicionar /quiz à lista de páginas pré-renderizadas

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     server: { entry: "server" },
     ...(isGitHubPages
       ? {
-          pages: [{ path: "/" }],
+          pages: [{ path: "/" }, { path: "/quiz" }],
           prerender: {
             enabled: true,
           },
